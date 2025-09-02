26778468da98aa42b9819897f2363eef1f14026e
Lucas Luciano Camino <capitanchorizo1234@gmail.com>
2025-09-02 15:31:57 -0300

    Ejercicios del 8 al 14 por Lucas Luciano Camino

---
+++ src/ar/edu/unju/escmi/tp2/ejercicio9/Ejercicio9.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.escmi.tp2.ejercicio9;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un año: ");
+        int anio = sc.nextInt();
+
+        boolean esBisiesto = false;
+
+        if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+            esBisiesto = true;
+        }
+
+        if (esBisiesto) {
+            System.out.println(anio + " es un año bisiesto.");
+        } else {
+            System.out.println(anio + " NO es un año bisiesto.");
+        }
+
+        sc.close();
+        
+    }
+
+}
